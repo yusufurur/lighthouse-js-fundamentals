103e26faaf4429ec66bb99f91b9886ca655d73b6
yusufurur <yusufurur@gmail.com>
2023-01-22 20:53:19 -0500

    Packing List:While

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,6 @@
-const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
-
 console.log("Kitchen stuff to pack:");
-for (let i = 0;i < packingList.length; i++) {
-  console.log(packingList[3]);
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
