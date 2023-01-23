17a55b77d5e10f7557ac93044ff817d5baa4fcba
yusufurur <yusufurur@gmail.com>
2023-01-22 19:55:07 -0500

    Even examples

---
+++ whichschool.js
@@ -0,0 +1,11 @@
+function whichSchool(age) {
+  if (age < 13) {
+    return "Elementary School";
+  }
+  else if (age >= 13 && age <=18) {
+     return "Secondary School";
+  }
+  else {
+    return "Lighthouse Labs";
+  }
+}
